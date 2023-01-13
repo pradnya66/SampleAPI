4439b9bd857608415c94907ba0cca3c3bcc12e35
vidhya <vidhya.ganesan88@gmail.com>
2023-01-12 21:11:52 -0600

    second commit

---
+++ src/test/java/base/BaseClass.java
@@ -0,0 +1,18 @@
+package base;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class BaseClass {
+	
+	public static final Logger logger = LogManager.getLogger(BaseClass.class.getName());
+
+
+	public String randomestring()
+	{
+		String generateinvalidID=RandomStringUtils.random(4);
+		return(generateinvalidID);
+	}
+
+}
--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,5 +1,23 @@
 package runner;
 
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = {"./src/test/resources/Feature"}, 
+				glue = {"stepDefinitions"},
+				monochrome = true, 
+				//dryRun = false, 
+				plugin = { "pretty",
+				"json:target/cucumber-reports/reports.json", 
+				"junit:target/cucumber-reports/Cucumber.xml",
+				"html:target/cucumber-reports/reports.html", 
+				"html:test-output/index.html" }, 
+				
+			
+				publish = true)
 public class TestRunner {
 
 }
