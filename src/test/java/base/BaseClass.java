7e3d484e956bfc1b50fe455869e2489b6862389c
Vidhya Ganeshan <vidhya.ganesan88@gmail.com>
2023-01-14 07:36:01 -0500

    Delete Batch Program changes

--- src/test/java/base/BaseClass.java
+++ src/test/java/base/BaseClass.java
@@ -1,18 +1,35 @@
 package base;
 
+import java.io.IOException;
+import java.util.Map;
+
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+
+import POJOs.ProgramDELETE;
+import utilities.Config;
+import utilities.ExcelReader;
 
 public class BaseClass {
 	
 	public static final Logger logger = LogManager.getLogger(BaseClass.class.getName());
-
+	public ProgramDELETE programDelete;
 
 	public String randomestring()
 	{
 		String generateinvalidID=RandomStringUtils.random(4);
 		return(generateinvalidID);
 	}
+	
+	public Map<String,String> getDataFromExcel(String SheetName, int Rownumber) throws InvalidFormatException, IOException {
+		ExcelReader excelReader = new ExcelReader();
+		Map<String,String> getData = 
+				excelReader.getData(Config.EXCEL, SheetName).get(Rownumber);
+		return getData;
+	}
+	
+	
 
 }
--- src/test/java/utilities/Config.java
+++ src/test/java/utilities/Config.java
@@ -5,5 +5,5 @@ public class Config {
 	public static final String BASE_URL = "https://lms-backend-service.herokuapp.com/lms";
 	public static final String GetAllProgram_URL = "https://lms-backend-service.herokuapp.com/lms/allPrograms";
 
-	public static final String EXCEL = "C:/Users/admin/eclipse-workspace/LMSRestAssuredAutomation/Data Files/LMSAPIHackathon.xlsx";
+	public static final String EXCEL = "Data Files/LMSAPIHackathon.xlsx";
 }
