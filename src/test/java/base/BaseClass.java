b546b74332a5014eeac161bcc8dd314e629626bd
Vidhya Ganeshan <vidhya.ganesan88@gmail.com>
2023-01-14 10:23:55 -0500

    Added excel file again

--- src/test/java/base/BaseClass.java
+++ src/test/java/base/BaseClass.java
@@ -8,14 +8,12 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
-import POJOs.ProgramDELETE;
 import utilities.Config;
 import utilities.ExcelReader;
 
 public class BaseClass {
 	
 	public static final Logger logger = LogManager.getLogger(BaseClass.class.getName());
-	public ProgramDELETE programDelete;
 
 	public String randomestring()
 	{
