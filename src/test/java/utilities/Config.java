e6d5b50cf6336d3b12efca35a8699a9e1497d29b
VidhyaGanesan1988 <vidhya.ganesan88@gmail.com>
2023-01-16 13:40:04 -0500

    Updated BatchPost, BatchPut, ProgramPut changes from Valli and Sapna

--- src/test/java/utilities/Config.java
+++ src/test/java/utilities/Config.java
@@ -13,7 +13,22 @@ public class Config {
 	public static final String GetSingleBatchName_URL = "https://lms-backend-service.herokuapp.com/lms/batches/batchName";
 	public static final String GetBatchByProgramId_URL = "https://lms-backend-service.herokuapp.com/lms/batches/program";
 
-
+	public static final String DeleteProgram_URL = "https://lms-backend-service.herokuapp.com/lms/deletebyprogid";
+	public static final String DeleteBatch_URL = "https://lms-backend-service.herokuapp.com/lms/batches";
+	
+	
 	public static final String EXCEL = "./Data Files/LMSAPIHackathon.xlsx";
+	
+	
+	public static final String BASE_Invalid_URL = "https://lms-backend.herokuapp.com/lms";
+	
+	public static final String Valid_BatchPut = "/batches/2442";
+	public static final String Invalid_BatchPut = "/batchessssssss/1456";
 
+	public static final String Invalid_Endpoint_PutProgram = "/putprogrammmmmm/329";
+	public static final String Valid_Endpoint_PutProgram = "/putprogram/4421";
+	public static final String ProgramId_NotOnServer = "/putprogram/8652";
+	
+	public static final String ValidEndPoint_ProgramName = "/program/jan23SDET1";
+	public static final String InValidEndPoint_ProgramName = "/program/SDET30255696563333333333";
 }
