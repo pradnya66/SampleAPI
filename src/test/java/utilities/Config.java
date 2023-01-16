4d1959a029aa355f1262bb5f572e349b92bfaef9
VidhyaGanesan1988 <vidhya.ganesan88@gmail.com>
2023-01-16 12:05:04 -0500

    Committing changes for BatchGet module

--- src/test/java/utilities/Config.java
+++ src/test/java/utilities/Config.java
@@ -8,6 +8,11 @@ public class Config {
 	public static final String PostProgram_URL = "https://lms-backend-service.herokuapp.com/lms/saveprogram";
 	public static final String PostBatch_URL = "https://lms-backend-service.herokuapp.com/lms/batches";
 
+	public static final String GetAllBatch_URL = "https://lms-backend-service.herokuapp.com/lms/batches";
+	public static final String GetSingleBatch_URL = "https://lms-backend-service.herokuapp.com/lms/batches/batchId";
+	public static final String GetSingleBatchName_URL = "https://lms-backend-service.herokuapp.com/lms/batches/batchName";
+	public static final String GetBatchByProgramId_URL = "https://lms-backend-service.herokuapp.com/lms/batches/program";
+
 
 	public static final String EXCEL = "./Data Files/LMSAPIHackathon.xlsx";
 
